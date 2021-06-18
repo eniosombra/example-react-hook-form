57ded45d4b5871bf2dbe82022e73f2b831db6589
Enio L. Sombra <eniosombra@gmail.com>
2021-06-18 10:16:25 -0300

    utilizando o react-hook-form de forma basica

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,31 @@
+import { useForm } from 'react-hook-form'
+
 import './App.css'
 
+type FormValues = {
+  name: string
+  email: string
+  group: string
+}
+
 function App() {
+  const { register, handleSubmit } = useForm<FormValues>()
+
+  const onSubmit = (data: any) => {
+    console.log(data)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Início da aplicação</h1>
-      </header>
+    <div className="App-header">
+      <h1>Cadastro de Cliente</h1>
+
+      <form onSubmit={handleSubmit(onSubmit)} autoComplete="off">
+        <input {...register('name')} />
+        <input {...register('email')} />
+        <input {...register('group')} />
+
+        <button type="submit">Cadastrar</button>
+      </form>
     </div>
   )
 }
